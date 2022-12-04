87d871955e1581f0ce2962a7b1037a6e4f014809
oscar.candelaria@millicom.com <oscar.candelaria@Millicom.com>
2022-12-04 17:25:32 -0600

    feat(Templates): change names lambdas

---
+++ src/lambdas/Lmdpublic/lambda_function.py
@@ -0,0 +1,24 @@
+import json
+import requests
+import psycopg2
+
+
+def lambda_handler(event, context):
+    r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=('user', 'pass'))
+    print(r.status_code)
+
+    try:
+        connection = psycopg2.connect(
+            host="dev-project-mydata-dbinstance.c8id9eg6dyio.us-east-1.rds.amazonaws.com",
+            user="postgres",
+            password="uzjmWhB5HVpxBNOSx8b.54Uu5JQNat",
+            database="mydata"
+        )
+        print("Conexión exitosa!!")
+    except Exception as ex:
+        print(ex)
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Mi lambda publica')
+    }
